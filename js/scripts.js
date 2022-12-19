c7bbf9d0521c933a317634b3b84800c000c92cbb
Antek93 <antonio.savino1993@gmail.com>
2022-12-19 14:50:17 +0100

    aggiunti i ;

--- js/scripts.js
+++ js/scripts.js
@@ -19,21 +19,21 @@ console.log('Distanza da percorrere:', distance);
 const yearOfBirth = prompt('Inserisci anno di nascita');
 console.log("Anno di nascità dell'utente", yearOfBirth);
 
-const now = new Date ()
+const now = new Date ();
 const age = (now.getFullYear() - yearOfBirth);
-console.log("Età dell'utente", age)
+console.log("Età dell'utente", age);
 
-let ticket_unit = 0.21
-let ticket_fair = (distance * ticket_unit)
+let ticket_unit = 0.21;
+let ticket_fair = (distance * ticket_unit);
 
 if (age > 18 && age < 65) {
-    console.log("Costo biglietto standard:", ticket_fair)
+    console.log("Costo biglietto standard:", ticket_fair);
 
 } else if (age < 18) {
-    let ticket_fair = (distance * ticket_unit) - ((distance * ticket_unit) * 20 / 100 )
+    let ticket_fair = (distance * ticket_unit) - ((distance * ticket_unit) * 20 / 100 );
     console.log("Costo biglietto sconto minorenni:", ((ticket_fair * 100) / 100) .toFixed(2));
 
 } else (age > 65) {
-    let ticket_fair = (distance * ticket_unit) - ((distance * ticket_unit) * 40 / 100 )
+    let ticket_fair = (distance * ticket_unit) - ((distance * ticket_unit) * 40 / 100 );
     console.log("Costo biglietto sconto anziani:", ((ticket_fair * 100) / 100) .toFixed(2));
 }
